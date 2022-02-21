6d075e8d1623b197fb70cc3c35a0d5ba9673fa85
Bojie Jiang <jiangbojie@gmail.com>
2022-02-21 11:48:51 -0800

    InputField

--- app/src/main/java/us/bojie/jettip/MainActivity.kt
+++ app/src/main/java/us/bojie/jettip/MainActivity.kt
@@ -8,18 +8,24 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import us.bojie.jettip.components.InputField
 import us.bojie.jettip.ui.theme.JetTipTheme
 
 class MainActivity : ComponentActivity() {
@@ -62,3 +68,33 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun MainContent() {
+    val (total, setTotal) = remember { mutableStateOf("") }
+    val valid = derivedStateOf { total.trim().isNotEmpty() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+
+    ) {
+        Column() {
+            InputField(
+                value = total,
+                onValueChange = setTotal,
+                labelId = "Enter Bill",
+                isSingleLine = true,
+                onAction = KeyboardActions{
+                    if (!valid.value) return@KeyboardActions
+                    keyboardController?.hide()
+                }
+            )
+        }
+    }
+}
--- app/src/main/java/us/bojie/jettip/components/Components.kt
+++ app/src/main/java/us/bojie/jettip/components/Components.kt
@@ -1,2 +1,50 @@
 package us.bojie.jettip.components
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AttachMoney
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun InputField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    labelId: String,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    isSingleLine: Boolean = false,
+    keyboardType: KeyboardType = KeyboardType.Number,
+    imeAction: ImeAction = ImeAction.Next,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+
+    OutlinedTextField(
+        modifier = modifier.padding(bottom = 10.dp, start = 10.dp, end = 10.dp),
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(text = labelId) },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Rounded.AttachMoney,
+                contentDescription = "Money Icon"
+            )
+        },
+        enabled = enabled,
+        singleLine = isSingleLine,
+        keyboardActions = onAction,
+        textStyle = TextStyle(fontSize = 18.sp, color = MaterialTheme.colors.onBackground),
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction)
+    )
+}
